d66df1e1d294adb6b4d4a0e537309d23cfeb9a2a
Ailson Freire <ailson_freire@outlook.com>
2017-07-16 22:54:31 -0300

    Cenas Star e SelecionarFase criadas, canvas de intrução tambem

---
+++ Assets/Scripts/GameOverLogic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverLogic : MonoBehaviour {
+
+    public int childCount;
+    public void countChildren()
+    {
+        GameObject buildBoard = GameObject.FindWithTag("Board");
+
+        childCount = buildBoard.transform.childCount;
+
+        Debug.Log(childCount);
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel() 
+    {
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void LoadDrugStoreLevel()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
