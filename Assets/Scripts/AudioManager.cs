a360492b0ff37a31163203fee7e9abe492129a03
Ailson Freire <ailson_freire@outlook.com>
2017-08-17 00:32:53 -0300

    Criaçao do Script de Som

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	static bool AudioBegin = false; 
+	void Awake()
+	{
+		if (!AudioBegin) {
+			
+			DontDestroyOnLoad (gameObject);
+			AudioBegin = true;
+		} 
+	}
+	void Update () {
+		if(Application.loadedLevelName == "Level 1 Farmacia")
+		{
+			
+			Destroy (this.gameObject);
+		}
+	}
+}
