21edfeb5d892e9bf2be2bb657a0998a6805f48d1
Ailson Freire <ailson_freire@outlook.com>
2017-08-19 21:36:24 -0300

    Criaçao do script que controla personagem pelo botão

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -13,6 +13,9 @@ public class PlayerControl : MonoBehaviour {
 	bool lastLeft = false;
 	bool lastRight = false;
 
+	bool stepLeft;
+	bool stepRight;
+
 	void Update () {
 
 		transform.position = new Vector3(-12.17f, -3.4f, transform.position.z);
@@ -25,15 +28,27 @@ public class PlayerControl : MonoBehaviour {
 			if(!lastLeft)
 				moveLeft (step);
 		}
+
+		if (stepLeft == true) {
+			if (!lastLeft)
+				moveLeft(step);
+		}
+
+		if (stepRight == true) {
+			if (!lastRight)
+				moveRight (step);
+		}
 	}
 		
 	public void moveRight (float step) {
+		stepRight = false;
 		lastLeft = false;
 		transform.position = Vector3.MoveTowards(transform.position, transform.position - Vector3.forward * lineWidth, step);
 
 	}
 
 	public void moveLeft (float step){
+		stepLeft = false;
 		lastRight = false;
 		transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.forward * lineWidth, step);
 	}
@@ -41,9 +56,20 @@ public class PlayerControl : MonoBehaviour {
 	void OnCollisionEnter(Collision collision)
 	{
 		transform.position = new Vector3(-12.17f, -3.4f, transform.position.z);
-		if (collision.gameObject.name == "Road1")
+		if (collision.gameObject.name == "Road1") {
 			lastLeft = true;
-		else if (collision.gameObject.name == "Road5")
+		} else if (collision.gameObject.name == "Road5") {
 			lastRight = true;
+		}
 	}
+
+	public void moveLeftTouch(){
+		stepLeft = true;
+	}
+
+	public void moveRightTouch(){
+		stepRight = true;
+	}
+
+
 }
