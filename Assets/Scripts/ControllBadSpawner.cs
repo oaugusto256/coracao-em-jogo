044eeb5b4cd0cc299e894b3299a3c5bc1958f43e
Ailson Freire <ailson_freire@outlook.com>
2017-10-06 00:02:15 -0300

    Script que evita 5 badSpawner numa mesma linha

---
+++ Assets/Scripts/ControllBadSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllBadSpawner : MonoBehaviour {
+
+    public GameObject BadSpawners;
+	GameObject[] ChildsOfBad;
+	GameObject[] listOfSpawner;
+	public int spawCicle;
+	int afectedSpawner;
+	float timeElapsed;
+
+	void Start () 
+	{
+		listOfSpawner = new GameObject[5];
+		ChildsOfBad = new GameObject[5];
+		spawCicle = Random.Range (0, 3);
+
+		for (int i = 0; i < 5; i++) 
+		{
+			listOfSpawner [i] = BadSpawners.transform.GetChild (i).gameObject;
+		} 
+	}
+	
+	void Update () 
+	{
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed > spawCicle) 
+		{	
+
+			ObjectPool objectPool = listOfSpawner [afectedSpawner].GetComponent<ObjectPool>();
+			objectPool.poolSize = 5;
+			afectedSpawner = Random.Range (0, 5);
+			print ("deactived " + afectedSpawner);
+			desactiveObject (afectedSpawner);
+			spawCicle = Random.Range (0, 3);
+			timeElapsed = 0;
+		}
+	}
+
+	void desactiveObject(int afectedSpawner)
+	{
+		ObjectPool objectPool = listOfSpawner [afectedSpawner].GetComponent<ObjectPool> ();
+		objectPool.poolSize = 0;
+	}
+}
