46c3c0ef8e77b02fb7f67e361a85be2a2f201405
Ailson Freire <ailson_freire@outlook.com>
2017-10-08 23:53:43 -0300

    Coloção de sons nos botões da cena Menu e Seleção de Fases

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour {
 
 	static bool AudioBegin = false; 
+
+
 	void Awake()
 	{
-		if (!AudioBegin) {
-			
+		if (!AudioBegin) 
+		{
 			DontDestroyOnLoad (gameObject);
 			AudioBegin = true;
 		} else {
@@ -18,11 +20,24 @@ public class AudioManager : MonoBehaviour {
 
 		}
 	}
-	void Update () {
+	void Update () 
+	{
 		if(Application.loadedLevelName == "Level 1 Farmacia")
 		{
 			AudioBegin = false;
 			Destroy (this.gameObject);
 		}
+		else if(Application.loadedLevelName == "Level 2 Supermercado")
+		{
+			AudioBegin = false;
+			Destroy (this.gameObject);
+		}
+		else if (Application.loadedLevelName == "Level 3 Academia ")
+		{
+			AudioBegin = false;
+			Destroy (this.gameObject);
+		}
 	}
+
+
 }
---
+++ Assets/Scripts/SoundsBtns.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsBtns : MonoBehaviour {
+
+	public AudioSource soundsButtom;
+	public AudioClip buttonGreen;
+	public AudioClip buttonRed;
+
+	public void clickButtonGreen()
+	{
+		StartCoroutine (SoundButtonGreen ());		
+	}
+
+	IEnumerator SoundButtonGreen ()
+	{
+		yield return new WaitForSeconds(0.01f);
+		soundsButtom.PlayOneShot(buttonGreen);
+	}
+
+	public void clickButtonRed()
+	{
+		StartCoroutine (SoundButtonRed());
+	}
+
+	IEnumerator SoundButtonRed()
+	{
+		yield return new WaitForSeconds(0.01f);
+		soundsButtom.PlayOneShot(buttonRed);
+	}
+}
