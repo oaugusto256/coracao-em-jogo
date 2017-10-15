a2c92bdf11d2f7bc75b272d34be23c212f7adb80
Ailson Freire <ailson_freire@outlook.com>
2017-10-14 21:53:05 -0300

    Configuração de som sendo salva e espalhada pra todas as cenas

--- Assets/Scripts/ConfigSounds.cs
+++ Assets/Scripts/ConfigSounds.cs
@@ -5,17 +5,93 @@ using UnityEngine.UI;
 
 public class ConfigSounds : MonoBehaviour {
 
-	public Slider configBtns;
-	float valueAtual;
-	float valuenterior;
+	GameObject findObj;
+	AudioSource music;
+	public Slider mscSounds;
+	public Slider btnSound;
+	public Slider sndEfects;
+	float atualMscSnds;
+	float antMscSnds;
+	float atualBtnValue;
+	float antBtnValue;
+	float atualSndEfct;
+	float antSndEfct;
 
 	void Start()
 	{
-		
+		findObj = GameObject.Find ("AudioManager");
+		music = findObj.GetComponent<AudioSource> ();
+
+		if( PlayerPrefs.HasKey ("mscValue") )
+		{
+			mscSounds.value = PlayerPrefs.GetFloat("mscValue");
+			music.volume = PlayerPrefs.GetFloat("mscValue");
+			antMscSnds = PlayerPrefs.GetFloat("mscValue");	
+		} else 
+		{
+			antMscSnds = 1;	
+		}
+
+		if( PlayerPrefs.HasKey ("btnValue") )
+		{
+			btnSound.value = PlayerPrefs.GetFloat("btnValue");
+			antBtnValue = PlayerPrefs.GetFloat("btnValue");	
+		} else 
+		{
+			antBtnValue = 1;	
+		}
+
+		if( PlayerPrefs.HasKey ("efcValue") )
+		{
+			sndEfects.value = PlayerPrefs.GetFloat("efcValue");
+			antSndEfct = PlayerPrefs.GetFloat("efcValue");	
+		} else 
+		{
+			antSndEfct = 1;	
+		}
 	}
 
-	void Update () 
+	void Update()
 	{
-		configBtns.value;	
+
+		atualMscSnds = mscSounds.value;
+
+		atualBtnValue = btnSound.value;
+
+		atualSndEfct = sndEfects.value;
+
+		if( antMscSnds != atualMscSnds )
+		{
+			saveNewMscValue ( atualMscSnds );
+			music.volume = atualMscSnds;
+			antMscSnds = atualMscSnds;
+		}
+
+		if( antBtnValue != atualBtnValue)
+		{
+			saveNewBtnValue( atualBtnValue);
+			antBtnValue = atualBtnValue;
+		}
+
+		if( antSndEfct != atualSndEfct )
+		{
+			saveNewEfcValue ( atualSndEfct );
+			antSndEfct = atualSndEfct;
+		}
+	}
+
+	void saveNewMscValue( float mscValue )
+	{
+		PlayerPrefs.SetFloat ("mscValue", mscValue);
+	}
+
+	void saveNewBtnValue( float btnValue)
+	{
+		PlayerPrefs.SetFloat ("btnValue", btnValue);
+	}
+
+	void saveNewEfcValue (float efcValue)
+	{
+		PlayerPrefs.SetFloat ("efcValue", efcValue);
 	}
 }
--- Assets/Scripts/ControllVolume.cs
+++ Assets/Scripts/ControllVolume.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class ControllVolume : MonoBehaviour {
 
+	public AudioSource mscSound;
 	public AudioSource btnGreen;
+	public AudioSource btnRed;
 
 	void Start ()
 	{
-			
+		if (PlayerPrefs.HasKey ("btnValue")) 
+		{
+			btnGreen.volume = PlayerPrefs.GetFloat ("btnValue");
+			btnRed.volume = PlayerPrefs.GetFloat ("btnValue");
+		}
+
+		if (PlayerPrefs.HasKey ("mscValue")) 
+		{
+			mscSound.volume = PlayerPrefs.GetFloat ("mscValue");
+		}
 	}
 }
