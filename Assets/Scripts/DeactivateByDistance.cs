16559c04dbe66624b421c2ed16cc4ddf6a08883a
Ailson Freire <ailson_freire@outlook.com>
2017-11-30 11:29:59 -0300

    Build Final, todos os ajutes

--- Assets/Scripts/DeactivateByDistance.cs
+++ Assets/Scripts/DeactivateByDistance.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeactivateByDistance : MonoBehaviour
 {
 	float distance = 0f;
-
+	
 	void Update () 
 	{
 		distance = Vector3.Distance(transform.parent.position, transform.position);
+			
 		if (distance > 350) 
 		{
 			gameObject.SetActive (false);
